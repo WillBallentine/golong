bdab8478a0e5579489f0651c805484cb3842f3af
willballentine <willh.ballentine@gmail.com>
2024-04-24 08:38:54 -0400

    queue creation basic

--- broker/broker.go
+++ broker/broker.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"github.com/gliderlabs/ssh"
 	"golang.org/x/term"
+	"regexp"
 	"sync"
 	"time"
 )
 
 var (
-	sessions map[*ssh.Session]*Broker
+	sessions    map[*ssh.Session]*Broker
+	newQueueCmd = regexp.MustCompile(`^/nq.*`)
+	helpCmd     = regexp.MustCompile(`^/help.*`)
+	exitCmd     = regexp.MustCompile(`^/exit.*`)
+	listCmd     = regexp.MustCompile(`^/list.*`)
 )
 
 type Broker struct {
@@ -45,46 +50,71 @@ func NewConsumer(broker *Broker) *Consumer {
 }
 
 func (b *Broker) Publish(message Message, name string) {
-	for i := range b.queues {
+	for i := 0; i < len(b.queues); i++ {
 		if name == b.queues[i].name {
 			b.mu.Lock()
 			defer b.mu.Unlock()
 			b.queues[i].messages = append(b.queues[i].messages, message)
+			fmt.Println("message added to queue\n")
+			break
 		}
 	}
 }
 
 func (b *Broker) Consume(name string) (Message, error) {
-	for i := range b.queues {
+	for i := 0; i < len(b.queues); i++ {
+		fmt.Printf("queue name: %s\n", name)
 		if name == b.queues[i].name {
 			b.mu.Lock()
 			defer b.mu.Unlock()
 			if len(b.queues[i].messages) == 0 {
-				return Message{}, errors.New("queue is empty")
+				return Message{}, errors.New("queue is empty\n")
 			}
 
 			message := b.queues[i].messages[0]
 			b.queues[i].messages = b.queues[i].messages[1:]
+			fmt.Println("message consumed\n")
 			return message, nil
 		}
 	}
-	fmt.Printf("queue name: %s", name)
 	return Message{Payload: []byte("queue not found")}, nil
 }
 
 func (b *Broker) SessionManager(sess ssh.Session) {
 	newTerm := term.NewTerminal(sess, fmt.Sprint("> "))
+	producer := NewProducer(b)
+	currentQueue := "init"
 	for {
+		fmt.Println("for loop sessionmanager")
+		fmt.Printf("queue is: %s\n", currentQueue)
 		line, err := newTerm.ReadLine()
 		if err != nil {
 			break
 		}
 
 		if len(line) > 0 {
-			//need to add in queue select
-			//need to add in queue create
-			producer := NewProducer(b)
-			producer.ProduceMessage([]byte(line), "test")
+			if string(line[0]) == "/" {
+				switch {
+				//need to add in queue select
+				case exitCmd.MatchString(string(line)):
+					return
+				case newQueueCmd.MatchString(string(line)):
+					newTerm.Write([]byte("enter new queue name: "))
+					name, err := newTerm.ReadLine()
+					if err != nil {
+						break
+					}
+					producer.ProduceMessage([]byte(line), name)
+					currentQueue = name
+					fmt.Println(currentQueue + "\n")
+				default:
+					producer.ProduceMessage([]byte(line), currentQueue)
+					fmt.Printf("default path. Queue = %s\n", currentQueue)
+				}
+			} else {
+				fmt.Println("else path")
+				producer.ProduceMessage([]byte(line), currentQueue)
+			}
 		}
 	}
 }
@@ -95,27 +125,26 @@ func NewProducer(broker *Broker) *Producer {
 
 func (p *Producer) ProduceMessage(payload []byte, name string) {
 	queueName := name
-	fmt.Println("beginning produce")
 	if len(p.Broker.queues) > 0 {
-		for i := range p.Broker.queues {
+		for i := 0; i < len(p.Broker.queues); i++ {
 			if p.Broker.queues[i].name == name {
+				fmt.Println("queue name found\n")
 				queueName = name
-				fmt.Println("using existing queue")
-				continue
+				break
 			} else {
-				fmt.Println("creating new queue")
+				fmt.Println("new queue created producer\n")
 				tmp := p.Broker.NewQueue(name)
 				p.Broker.queues = append(p.Broker.queues, tmp)
 				queueName = tmp.name
 			}
 		}
 	} else {
-		fmt.Println("produce else block")
+		fmt.Println("queues list was empty, creating new\n")
 		tmp := p.Broker.NewQueue(name)
 		p.Broker.queues = append(p.Broker.queues, tmp)
 		queueName = tmp.name
 	}
-	fmt.Println("creating message")
+	fmt.Println("message being created\n")
 	messageID := fmt.Sprintf("message - %d", time.Now().UnixNano())
 	message := Message{ID: messageID, Payload: payload}
 
